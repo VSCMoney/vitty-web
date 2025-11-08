f28946af0fa826a22659b9fc238b3d0c55565e89
piyushgoyal <piyush@vsc.co.in>
2025-11-08 13:24:24 +0530

    Fixed bug
    Signed-off-by: piyushgoyal <piyush@vsc.co.in>

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import "./styles/app.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// GitHub Pages ke liye basename set:
+// local pe "/" rahega, production pe "/vitty-web"
+const basename =
+  process.env.NODE_ENV === "production" ? "/vitty-web" : "/";
+
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
